36a5cd95df028ef51cd3441efa5a9b02a11dfe69
Veta <elizavetamariazuenko@gmail.com>
2025-02-22 15:21:58 +0700

    n

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
 print("My first git-repo")
+#cd Desktop/Питон/git/
+# git clone https://github.com/LVeta/bubugaga.git
+# cd bubugaga
+# python -m venv .venv
+# git config --global user.email elizavetamariazuenko@gmail.com
+# git config --global user.name Veta
+# cd .. вернутся на уровень выше
+
+#vetka nobaia
